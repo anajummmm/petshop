15462935ba73ac6c5390f949dd710e8f72914f78
anajummmm <165791915+anajummmm@users.noreply.github.com>
2025-04-29 13:56:55 -0300

    aa

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify, request
+from models.produtos import products
+from config.jwt_config import token_required
+
+app = Flask(__name__)
+
+@app.route('/products')
+@token_required
+def listar_produtos():
+    preco_asc = request.args.get('preco_asc')
+    preco_desc = request.args.get('preco_desc')
+    desc_part = request.args.get('description_part')
+
+    lista = products.copy()
+
+    if preco_asc:
+        lista.sort(key=lambda x: x['product_price'])
+    elif preco_desc:
+        lista.sort(key=lambda x: x['product_price'], reverse=True)
+    elif desc_part:
+        lista = [p for p in lista if desc_part.lower() in p['product_description'].lower()]
+
+    return jsonify(lista)
+
+@app.route('/products/<int:id>')
+@token_required
+def get_produto(id):
+    for produto in products:
+        if produto["id"] == id:
+            return jsonify(produto)
+    return jsonify({'message': 'produto nao encontrado'}), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
